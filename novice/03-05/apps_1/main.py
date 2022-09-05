241aa3a4170b10393ebcde6898b4d7d2f994f854
nazilcerdas <111473501+nazilcerdas@users.noreply.github.com>
2022-09-06 05:41:14 +0700

    Add files via upload

---
+++ novice/03-05/apps_1/main.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request, redirect
+import psycopg2
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+
+    conn = psycopg2.connect(
+        host="localhost",
+        database="contoh",
+        user="postgres",
+        password="nazil-280103")
+    curs = conn.cursor()
+    if request.method == "POST":
+        nama = request.form.get("nama")
+        detail = request.form.get("detail")
+        query = f"insert into buah(nama, detail) values('{nama}', '{detail}')"
+        curs.execute(query)
+        conn.commit()
+        curs.close()
+        conn.close()
+        # print(20*"=")
+        # print(nama)
+        # print(detail)
+        # print(20*"=")
+
+    print(request.method)
+    query = f"select * from buah"
+    curs.execute(query)
+    data = curs.fetchall()
+
+    # data = ["apel", "pear", "anggur"]
+    return render_template("index.html", context = data)
+
+@app.route("/detail/<buah_id>")
+def detail(buah_id):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="contoh",
+        user="postgres",
+        password="nazil-280103")
+    
+    curs = conn.cursor()
+    query = f"select * from buah where id = {buah_id}"
+    curs.execute(query)
+    data = curs.fetchone()
+    # conn.close()
+    curs.close()
+    conn.close()
+    print(data)
+    return render_template("detail.html", context=data)
+
+@app.route("/delete/<buah_id>")
+def delete(buah_id):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="contoh",
+        user="postgres",
+        password="nazil-280103")
+    
+    curs = conn.cursor()
+    # if request.method == "POST":
+    #     nama = request.form.get("nama")
+    #     detail = request.form.get("detail")
+    query = f"delete from buah where id = {buah_id}"
+    curs.execute(query)
+    conn.commit()
+    curs.close()
+    conn.close()
+    return redirect("/")
+
+
+
+if __name__ == "__main__":
+    app.run()
